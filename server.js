4e7720737a4edde553f0c7cf9a6e192931cd4a6d
Edward England <edward_tx@edwebs.net>
2023-11-12 12:49:33 -0600

    Connected to mongo database

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
+const dotenv = require('dotenv') // .env file
+dotenv.config() // using .env
+const connectionString = `mongodb+srv://${process.env.USER}:${process.env.PW}@cluster0.9accb.mongodb.net/starwars2023?retryWrites=true&w=majority`
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
 // Connect to Database
-MongoClient.connect('mongodb-connection-string', (err, client) => {
-    // ... do something here
+MongoClient.connect(connectionString)
+.then(client => {
+    console.log('Connected to Database')
+
+    // MIDDLEWARE
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    // All your handlers here...
+    app.get('/', (req, res) => {
+        //res.send('Hello World')
+        res.sendFile(__dirname + '/index.html')
+        // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+        // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+        // Mine: C:\Users\eengl\OneDrive\Documents\htdocs\bootcamp\zellwk-com-crud\starwars2023
+    })
+
+    app.post('/quotes', (req, res) => {
+        //console.log('Helloooooooooooooo!')
+        console.log(req.body)
+    })
+
+
+    app.listen(3000, function () {
+        //console.log(`__dirname is: ${__dirname}`)
+        console.log('Listening on 3000')
+    })
 })
+.catch(error => console.error(error))
 
-// MIDDLEWARE
-// Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// All your handlers here...
-app.get('/', (req, res) => {
-    //res.send('Hello World')
-    res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-    // Mine: C:\Users\eengl\OneDrive\Documents\htdocs\bootcamp\zellwk-com-crud\starwars2023
-})
-
-app.post('/quotes', (req, res) => {
-    //console.log('Helloooooooooooooo!')
-    console.log(req.body)
-})
-
-
-app.listen(3000, function () {
-    console.log(`__dirname is: ${__dirname}`)
-    console.log('Listening on 3000')
-})
