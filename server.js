84426789948b68296634f4056ce437fa3f4fde55
Edward England <edward_tx@edwebs.net>
2023-11-12 12:19:54 -0600

    app.get with Hello World response

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
 app.listen(3000, function () {
     console.log('Listening on 3000')
 })
