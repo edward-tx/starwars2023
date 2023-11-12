27385f6702cf7863183c45a8cb8b779af81eb76f
Edward England <edward_tx@edwebs.net>
2023-11-12 12:24:38 -0600

    Use sendFile to serve up index.html

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    //res.send('Hello World')
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+    // Mine: C:\Users\eengl\OneDrive\Documents\htdocs\bootcamp\zellwk-com-crud\starwars2023
 })
 
 
 app.listen(3000, function () {
+    console.log(`__dirname is: ${__dirname}`)
     console.log('Listening on 3000')
 })
