5ed4b9b7e4a973dcffd7ccd5c33764857eb52d1d
Edward England <edward_tx@edwebs.net>
2023-11-12 12:34:08 -0600

    Install body-parser, send form values to console

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// All your handlers here...
 app.get('/', (req, res) => {
     //res.send('Hello World')
     res.sendFile(__dirname + '/index.html')
@@ -11,7 +15,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log('Helloooooooooooooo!')
+    //console.log('Helloooooooooooooo!')
+    console.log(req.body)
 })
 
 
